eb20fe017256737cd42c56cbb537765a7a35f153
maneeshtiwari007 <maneeshtiwari007@gmail.com>
2025-07-31 13:47:37 +0530

    commit changes
    commit changes

--- config/env.ts
+++ config/env.ts
@@ -1,6 +1,6 @@
 // Environment variables configuration
 export const ENV = {
-  NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: 'AIzaSyBXHWWYgIaeDqaTugd6QTlAmW2OYoYiJkw'//'AIzaSyAezq4Oknv0Z84SyC1l8CJN5QtNfr6KetQ',
+  NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: 'AIzaSyBD3jlRyXvilfXTs0572Z7rYmCAZIbzvb0'//'AIzaSyAezq4Oknv0Z84SyC1l8CJN5QtNfr6KetQ',
 };
  
 // Make ENV available on window for client-side access
